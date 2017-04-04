2ff39c0e74a2d55df239774f704131532fb18a71
Tyler Long <tyler4long@gmail.com>
2017-04-04 19:37:05 +0800

    Only bundle test project

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
+import path from 'path'
 import nodeExternals from 'webpack-node-externals'
 
 const config = {
   target: 'node',
   externals: [nodeExternals()],
   entry: {
-    'src/index': './src/index.js',
-    'test/index': './test/index.js'
+    'index': './test/index.js'
   },
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'test'),
     filename: '[name].bundle.js',
     libraryTarget: 'commonjs2'
   },
